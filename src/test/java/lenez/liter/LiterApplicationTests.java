67a7fcab1740c96c72b6b3418a81abd0a14a44e8
leneza41 <leonar4145@gmail.com>
2024-07-13 10:35:18 -0600

    Initial commit

---
+++ src/test/java/lenez/liter/LiterApplicationTests.java
@@ -0,0 +1,13 @@
+package lenez.liter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
